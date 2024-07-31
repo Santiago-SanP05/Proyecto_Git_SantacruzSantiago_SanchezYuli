1e70f98d860850965b8aa35ae2b5c4f73da79512
Santiago-SanP05 <santiagopinzoncruz@gmail.com>
2024-07-31 11:04:49 -0500

    feat: :sparkles: Se agrega un nuevo menu y se corrige el menu principal(se le agrega otra función)
    El nuevo menu se crea para generar un orden y experincia de usuario al usar el programa y para ser mas ordenado

--- Menu.py
+++ Menu.py
@@ -3,7 +3,7 @@
 
 
 def Menu():
-    list = ["1.para ingresar un usuario nuevo","2.para editar","3.para salir"]
+    list = ["1.para ingresar un usuario nuevo","2.para editar","3.para busqueda avanzada","4.para salir"]
     while True:
         print("por favor marca una de las opcciones")
         for i in list:
@@ -13,10 +13,27 @@ def Menu():
             print("---accediendo---")
         elif opc == 2:
             print("---accediendo---")
+        elif opc == 3:
+            print("---accediendo---")            
         elif opc == len(list):
             print("---Cerrando Programa---")
             print("hasta la proxima...")
             break
 
+def Menutwo():
+    list = ["1.Cod-postal","2.poblacio Estimada",".3Pais de origen","Ó si no marca 4 para salir"]
+    while True:
+        print("por favor selecciones una opccion a hacer Busqueda avanzada")
+        for i in list:
+            print(i)
+        opc == int(input("->"))
+        if opc == 1:
+            print("---Accediendo---")
+        elif opc == 2:
+            print("---Accediendo---")
+        elif opc == 3:
+            print("---Accediendo---")
+        elif opc == len(list):
+            print("----Retrocediendo----")         
 
         
